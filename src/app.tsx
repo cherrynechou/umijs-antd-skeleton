c3da87c14314f168d1fe459a7b6558ee7c23e909
cherrynechou <iboy_2008@163.com>
2024-11-20 18:34:23 +0800

    wip

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,11 @@
 
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
 // 更多信息见文档：https://umijs.org/docs/api/runtime-config#getinitialstate
+
+const isDev = process.env.NODE_ENV === 'development';
+const loginPath = '/admin/login';
+
+
 export async function getInitialState(): Promise<{ name: string }> {
   return { name: '@umijs/max' };
 }
@@ -9,6 +14,7 @@ export async function getInitialState(): Promise<{ name: string }> {
 export const layout = () => {
   return {
     logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
+    layout: 'mix',
     menu: {
       locale: false,
     },
